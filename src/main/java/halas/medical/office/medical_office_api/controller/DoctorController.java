1813e60689db61604f1ed401e08095b219a0a609
Alexandre Halas <alexandrehalas@gmail.com>
2024-12-08 20:23:50 -0300

    feat: New endpoint get doctor list

--- src/main/java/halas/medical/office/medical_office_api/controller/DoctorController.java
+++ src/main/java/halas/medical/office/medical_office_api/controller/DoctorController.java
@@ -3,13 +3,13 @@ package halas.medical.office.medical_office_api.controller;
 import halas.medical.office.medical_office_api.doctor.Doctor;
 import halas.medical.office.medical_office_api.doctor.DoctorDto;
 import halas.medical.office.medical_office_api.doctor.DoctorRepository;
+import halas.medical.office.medical_office_api.doctor.DoctorResponseListDto;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("doctors")
@@ -22,6 +22,11 @@ public class DoctorController {
     @Transactional
     public void register(@RequestBody @Valid DoctorDto doctorDto) {
         doctorRepository.save(new Doctor(doctorDto));
+    }
+
+    @GetMapping
+    public List<DoctorResponseListDto> list() {
+        return doctorRepository.findAll().stream().map(DoctorResponseListDto::new).toList();
     }
 
 }
