62b50841f682ffff6579b8846516b1717c847688
Alexandre Halas <alexandrehalas@gmail.com>
2024-12-08 00:01:16 -0300

    feat: Initialized Spring Boot Project

---
+++ src/main/java/halas/medical/office/medical_office_api/MedicalOfficeApiApplication.java
@@ -0,0 +1,13 @@
+package halas.medical.office.medical_office_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalOfficeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalOfficeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/halas/medical/office/medical_office_api/MedicalOfficeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package halas.medical.office.medical_office_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalOfficeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
