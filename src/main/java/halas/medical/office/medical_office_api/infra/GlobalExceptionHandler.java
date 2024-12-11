4eb8a312b291e940dfe9a35f4baa7cf44ba9f3b6
Alexandre Halas <alexandrehalas@gmail.com>
2024-12-10 22:16:41 -0300

    feat: Implemented a GlobalExceptionHandler to handle errors of not found and bad requests

---
+++ src/main/java/halas/medical/office/medical_office_api/infra/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package halas.medical.office.medical_office_api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> errorHandlerNotFound() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> errorHandlerBadRequest(MethodArgumentNotValidException exception) {
+        var fieldErrors = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(fieldErrors.stream().map(ErrorValidationData::new));
+    }
+
+    private record ErrorValidationData(String field, String message) {
+        public ErrorValidationData(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
+}
